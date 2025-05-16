aae57912dfffbf40bf749aea5f1dd222afcec5cb
Denisse Chagoya <denissechagoyaveloz@gmail.com>
2025-05-16 17:28:12 -0600

    ejemplo practico de el uso de principio de apertura y cierre O

--- app/src/main/java/com/example/cryptoapp/patterns/OCP.kt
+++ app/src/main/java/com/example/cryptoapp/patterns/OCP.kt
@@ -1,2 +1,32 @@
 package com.example.cryptoapp.patterns
 
+// abierto- cerrado
+interface Figura {
+    fun calcularArea(): Double
+}
+
+// Implementaciones
+class Circulo(val radio: Double) : Figura {
+    override fun calcularArea() = Math.PI * radio * radio
+}
+
+class Rectangulo(val ancho: Double, val alto: Double) : Figura {
+    override fun calcularArea() = ancho * alto
+}
+
+// Clase que usa la interfaz
+fun mostrarArea(figura: Figura) {
+    println("El área es: ${figura.calcularArea()}")
+}
+
+
+
+fun main() {
+    val circulo = Circulo(3.0)
+    val rectangulo = Rectangulo(4.0, 5.0)
+
+    mostrarArea(circulo)
+    mostrarArea(rectangulo)
+}
+
+
